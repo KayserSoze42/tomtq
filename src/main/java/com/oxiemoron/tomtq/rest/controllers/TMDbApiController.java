1f838613670d5e3f4b517296b555b94195145363
KayserSoze42 <morus.hana@gmail.com>
2023-06-01 08:57:58 +0200

    Rest je za trest
    Modified /show and /movie mappings logic in RestController
    
    Added option to find shows and movies using discover endpoint and with_cast parameter.
    By supplying cast param to either /show or /movie it will return a list of found items with given filter
    
    eg. /movie?cast=Samuel+L+Jackson,Bruce+Willis

--- src/main/java/com/oxiemoron/tomtq/rest/controllers/TMDbApiController.java
+++ src/main/java/com/oxiemoron/tomtq/rest/controllers/TMDbApiController.java
@@ -1,11 +1,14 @@
 package com.oxiemoron.tomtq.rest.controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.oxiemoron.tomtq.rest.models.details.movie.MovieDetailsResponse;
 import com.oxiemoron.tomtq.rest.models.details.show.ShowDetailsResponse;
 import com.oxiemoron.tomtq.rest.models.search.movie.MovieSearchResponse;
 import com.oxiemoron.tomtq.rest.models.search.movie.MovieSearchResult;
+import com.oxiemoron.tomtq.rest.models.search.person.PersonSearchResponse;
+import com.oxiemoron.tomtq.rest.models.search.person.PersonSearchResult;
 import com.oxiemoron.tomtq.rest.models.search.show.ShowSearchResponse;
 import com.oxiemoron.tomtq.rest.models.search.show.ShowSearchResult;
 
@@ -21,7 +24,37 @@ public class TMDbApiController {
     private static final String SEARCH_URL = "https://api.themoviedb.org/3/search/%s?api_key=%s&query=%s";
     private static final String DETAILS_URL = "https://api.themoviedb.org/3/%s/%d?api_key=%s";
 
-    public static ObjectNode searchMovieByTitle(String title) {
+    private static final String DISCOVER_URL = "https://api.themoviedb.org/3/discover/%s?api_key=%s&with_cast=%s";
+
+
+
+    public static ObjectNode searchPersonByName(String name) {
+
+        String queryUrl = String.format(SEARCH_URL, "person", API_KEY, name.replace(" ", "+"));
+        PersonSearchResponse response;
+        String jsonOutput = "{N/A}";
+        ObjectNode output = objectMapper.createObjectNode();
+        try {
+            response = objectMapper.readValue(new URL(queryUrl), PersonSearchResponse.class);
+            ArrayList<PersonSearchResult> results = response.getResults();
+
+            int personId = results.get(0).getId();
+            String personName = results.get(0).getName();
+
+            output.put("id", personId);
+            output.put("original_name", personName);
+
+
+        } catch (IOException ioE) {
+            ioE.printStackTrace();
+        } catch (IndexOutOfBoundsException iobE) {
+            System.out.println(iobE.toString());
+        }
+        return output;
+
+    }
+
+    public static ObjectNode searchMovieWithTitle(String title) {
 
         String queryUrl = String.format(SEARCH_URL, "movie", API_KEY, title.replace(" ", "+"));
         MovieSearchResponse response;
@@ -56,7 +89,60 @@ public class TMDbApiController {
         return outputs;
     }
 
-    public static ObjectNode searchShowByTitle(String title) {
+    public static ObjectNode searchMoviesWithCast(String cast) {
+
+        StringBuilder castQuery = new StringBuilder();
+        String delimiter = "";
+
+        for (String person : cast.split(",")) {
+
+            ObjectNode currentPerson = searchPersonByName(person.replace(" ", "+"));
+            int currentId = currentPerson.get("id").asInt();
+            castQuery.append(delimiter);
+            castQuery.append(currentId);
+            delimiter = ",";
+
+        }
+
+        String queryUrl = String.format(DISCOVER_URL, "movie", API_KEY, castQuery.toString());
+        MovieSearchResponse response;
+        String jsonOutputs = "{N/A}";
+        ObjectNode outputs = objectMapper.createObjectNode();
+
+        try {
+
+            response = objectMapper.readValue(new URL(queryUrl), MovieSearchResponse.class);
+            ArrayList<MovieSearchResult> results = response.getResults();
+
+            int resultNo = 0;
+
+            for (MovieSearchResult result : results) {
+                resultNo += 1;
+
+                ObjectNode output = objectMapper.createObjectNode();
+
+                output.put("id", result.getId());
+                output.put("title", result.getTitle());
+                output.put("release_date", result.getRelease_date());
+                output.put("overview", result.getOverview());
+
+                String jsonOutput = objectMapper.writeValueAsString(output);
+
+                outputs.put(Integer.toString(resultNo), jsonOutput);
+            }
+
+            jsonOutputs = objectMapper.writeValueAsString(outputs);
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        return outputs;
+
+
+    }
+
+    public static ObjectNode searchShowWithTitle(String title) {
 
         String queryUrl = String.format(SEARCH_URL, "tv", API_KEY, title.replace(" ", "+"));
         ShowSearchResponse response;
@@ -89,6 +175,59 @@ public class TMDbApiController {
             ioE.printStackTrace();
         }
         return outputs;
+    }
+
+    public static ObjectNode searchShowWithCast(String cast) {
+
+        StringBuilder castQuery = new StringBuilder();
+        String delimiter = "";
+
+        for (String person : cast.split(",")) {
+
+            ObjectNode currentPerson = searchPersonByName(person.replace(" ", "+"));
+            int currentId = currentPerson.get("id").asInt();
+            castQuery.append(delimiter);
+            castQuery.append(currentId);
+            delimiter = ",";
+
+        }
+
+        String queryUrl = String.format(DISCOVER_URL, "tv", API_KEY, castQuery.toString());
+        ShowSearchResponse response;
+        String jsonOutputs = "{N/A}";
+        ObjectNode outputs = objectMapper.createObjectNode();
+
+        try {
+
+            response = objectMapper.readValue(new URL(queryUrl), ShowSearchResponse.class);
+            ArrayList<ShowSearchResult> results = response.getResults();
+
+            int resultNo = 0;
+
+            for (ShowSearchResult result : results) {
+                resultNo += 1;
+
+                ObjectNode output = objectMapper.createObjectNode();
+
+                output.put("id", result.getId());
+                output.put("name", result.getName());
+                output.put("first_air_date", result.getFirst_air_date());
+                output.put("overview", result.getOverview());
+
+                String jsonOutput = objectMapper.writeValueAsString(output);
+
+                outputs.put(Integer.toString(resultNo), jsonOutput);
+            }
+
+            jsonOutputs = objectMapper.writeValueAsString(outputs);
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        return outputs;
+
+
     }
 
     public static ObjectNode getMovieById(int id) {
--- src/main/java/com/oxiemoron/tomtq/rest/controllers/TOMTQRestController.java
+++ src/main/java/com/oxiemoron/tomtq/rest/controllers/TOMTQRestController.java
@@ -6,13 +6,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 public class TOMTQRestController {
 
-
     @GetMapping("/tomtq/movie")
-    public ObjectNode findMovieByTitle(@RequestParam(value = "title", defaultValue = "Lord Of The Rings") String title) {
-        return TMDbApiController.searchMovieByTitle(title);
+    public ObjectNode findMovieByTitle(@RequestParam(value = "title", required = false) String title,
+                                       @RequestParam(value = "cast", required = false) String cast) {
+
+        if (title != null) {
+            return TMDbApiController.searchMovieWithTitle(title);
+        } else if (cast != null) {
+            return TMDbApiController.searchMoviesWithCast(cast);
+        } else {
+            return TMDbApiController.searchMovieWithTitle("Lord Of The Rings");
+        }
+
     }
 
     @GetMapping("/tomtq/movie/{id}")
@@ -21,12 +31,26 @@ public class TOMTQRestController {
     }
 
     @GetMapping("/tomtq/show")
-    public ObjectNode findShowByTitle(@RequestParam(value = "title", defaultValue = "The Good Place") String title) {
-        return TMDbApiController.searchShowByTitle(title);
+    public ObjectNode findShowByTitle(@RequestParam(value = "title", required = false) String title,
+                                       @RequestParam(value = "cast", required = false) String cast) {
+
+        if (title != null) {
+            return TMDbApiController.searchShowWithTitle(title);
+        } else if (cast != null) {
+            return TMDbApiController.searchShowWithCast(cast);
+        } else {
+            return TMDbApiController.searchShowWithTitle("The Good Place");
+        }
+
     }
 
     @GetMapping("/tomtq/show/{id}")
     public ObjectNode getShowById(@PathVariable(value = "id") int id) {
         return TMDbApiController.getShowById(id);
+    }
+
+    @GetMapping("/tomtq/person")
+    public ObjectNode findPersonByName(@RequestParam(value = "query", defaultValue = "Robert Glenister") String query) {
+        return  TMDbApiController.searchPersonByName(query);
     }
 }
