b7d759660adfe73117fa2c593dcc5e4b2c79c748
KayserSoze42 <morus.hana@gmail.com>
2023-07-08 07:45:39 +0200

    move the books, leave the REST
    watch out for the..
    
    ..ouch

--- src/main/java/ink/oxiemoron/tomtq/lexicon/lateral/Tokens.java
+++
@@ -1,5 +0,0 @@
-package ink.oxiemoron.tomtq.lexicon.lateral;
-
-public enum Tokens {
-    Show, Movie, Person // I don't like people. I like rock and roll, lexers and pizza. In that order. ;) haha
-}
